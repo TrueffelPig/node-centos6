6229843a5c2875fa5931374fcbb2e1ef4022fc4d
Maurits Lamers <mauritslamers@gmail.com>
2022-04-01 17:50:08 +0200

    removing configure param as it causes the build to break

--- .github/actions/build-node-action/make-node.sh
+++ .github/actions/build-node-action/make-node.sh
@@ -18,6 +18,6 @@ cd ${NODE_DIR}
 export CPPFLAGS=-D__STDC_FORMAT_MACROS
 export LDFLAGS=-lrt
 
-scl enable devtoolset-7 rh-python36 "./configure --without-os-feature=getauxval"
+scl enable devtoolset-7 rh-python36 ./configure
 scl enable devtoolset-7 rh-python36 "ARCH=x64 make -j$(nproc) binary"
 sha256sum node-${NODE_VERSION}-linux-x64.tar.xz node-${NODE_VERSION}-linux-x64.tar.gz > SHASUMS256.txt
