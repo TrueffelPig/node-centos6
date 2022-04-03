4e353fe5dddef8ca389535c8575a5d1554f454a8
Maurits Lamers <mlamers@turnitin.com>
2022-04-03 22:19:35 +0200

    rewrite the packing to copy the nodejs approach

--- .github/actions/build-node-action/make-node.sh
+++ .github/actions/build-node-action/make-node.sh
@@ -31,8 +31,6 @@ tar -xvzf patchelf-0.14.5-x86_64.tar.gz
 
 tar -xvzf ${GITHUB_WORKSPACE}/${NODE_DIR}/node-${NODE_VERSION}-linux-x64.tar.gz
 
-find ${GITHUB_WORKSPACE}
-
 cd node-${NODE_VERSION}-linux-x64
 cp -R /lib64 ./lib64
 cd bin
@@ -40,6 +38,9 @@ cd bin
 
 ## now repack
 cd ${GITHUB_WORKSPACE}/centos_patch
-tar -cvzf node-${NODE_VERSION}-linux-x64.tar.gz node-${NODE_VERSION}-linux-x64
-tar -cvJf node-${NODE_VERSION}-linux-x64.tar.xz node-${NODE_VERSION}-linux-x64
+tar -cf node-${NODE_VERSION}-linux-x64.tar node-${NODE_VERSION}-linux-x64
+rm -f -r node-${NODE_VERSION}-linux-x64
+gzip -c -f -9 node-${NODE_VERSION}-linux-x64.tar > node-${NODE_VERSION}-linux-x64.tar.gz
+xz -c -f -9e node-${NODE_VERSION}-linux-x64.tar > node-${NODE_VERSION}-linux-x64.tar.xz
+rm -f node-${NODE_VERSION}-linux-x64.tar
 sha256sum node-${NODE_VERSION}-linux-x64.tar.xz node-${NODE_VERSION}-linux-x64.tar.gz > SHASUMS256.txt
