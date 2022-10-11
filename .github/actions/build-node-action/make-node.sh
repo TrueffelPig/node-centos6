97defc56dd865e0d056202e88306d78d586d7efd
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2022-10-11 10:33:45 +0300

    Update make-node.sh

--- .github/actions/build-node-action/make-node.sh
+++ .github/actions/build-node-action/make-node.sh
@@ -18,30 +18,6 @@ cd ${NODE_DIR}
 export CPPFLAGS=-D__STDC_FORMAT_MACROS
 export LDFLAGS=-lrt
 
-scl enable devtoolset-9 rh-python36 "./configure --fully-static --enable-static"
-scl enable devtoolset-9 rh-python36 "ARCH=x64 make -j$(nproc) binary"
-
-cd ${GITHUB_WORKSPACE}
-
-mkdir centos_patch
-cd centos_patch
-
-wget https://github.com/NixOS/patchelf/releases/download/0.14.5/patchelf-0.14.5-x86_64.tar.gz
-## this unpacks in the current folder
-tar -xvzf patchelf-0.14.5-x86_64.tar.gz
-
-tar -xvzf ${GITHUB_WORKSPACE}/${NODE_DIR}/node-${NODE_VERSION}-linux-x64.tar.gz
-
-cd node-${NODE_VERSION}-linux-x64
-cp -R /lib64 ./lib64
-cd bin
-../../bin/patchelf --set-interpreter ../lib64/ld-linux-x86-64.so.2 --set-rpath ../lib64 node 
-
-## now repack
-cd ${GITHUB_WORKSPACE}/centos_patch
-tar -cf node-${NODE_VERSION}-linux-x64.tar node-${NODE_VERSION}-linux-x64
-rm -f -r node-${NODE_VERSION}-linux-x64
-gzip -c -f -9 node-${NODE_VERSION}-linux-x64.tar > node-${NODE_VERSION}-linux-x64.tar.gz
-xz -c -f -9e node-${NODE_VERSION}-linux-x64.tar > node-${NODE_VERSION}-linux-x64.tar.xz
-rm -f node-${NODE_VERSION}-linux-x64.tar
+scl enable devtoolset-6 python27 ./configure
+scl enable devtoolset-6 python27 "ARCH=x64 make -j$(nproc) binary"
 sha256sum node-${NODE_VERSION}-linux-x64.tar.xz node-${NODE_VERSION}-linux-x64.tar.gz > SHASUMS256.txt
